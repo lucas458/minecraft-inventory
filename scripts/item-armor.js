9e975e788b355bb7850852f72d17f8a784b60564
lucas458 <lucasvictor1111@gmail.com>
2025-10-04 02:06:56 -0300

    refactor: Separação dos tipos de itens em arquivos separados

---
+++ scripts/item-armor.js
@@ -0,0 +1,27 @@
+const MINECRAFT_ARMOR = {
+
+    "leather_helmet": "helmet",
+    "chainmail_helmet": "helmet",
+    "iron_helmet": "helmet",
+    "golden_helmet": "helmet",
+    "diamond_helmet": "helmet",
+    
+    "leather_chestplate": "chestplate",
+    "chainmail_chestplate": "chestplate",
+    "iron_chestplate": "chestplate",
+    "golden_chestplate": "chestplate",
+    "diamond_chestplate": "chestplate",
+
+    "leather_leggings": "leggings",
+    "chainmail_leggings": "leggings",
+    "iron_leggings": "leggings",
+    "golden_leggings": "leggings",
+    "diamond_leggings": "leggings",
+
+    "leather_boots": "boots",
+    "chainmail_boots": "boots",
+    "iron_boots": "boots",
+    "golden_boots": "boots",
+    "diamond_boots": "boots"
+
+};
